d5d09962aea64fd7f22fa6b3935367e5b87c8321
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-01 15:50:37 -0600

    Remove user story comments.

--- spec/features/shelter_pets/shelter_can_create_a_pet_spec.rb
+++ spec/features/shelter_pets/shelter_can_create_a_pet_spec.rb
@@ -39,20 +39,3 @@ RSpec.describe 'Shelter can create a pet' do
     expect(page).to have_content("percy.jpg")
   end
 end
-
-# As a visitor
-# When I visit a Shelter Pets Index page
-# Then I see a link to add a new adoptable pet for that shelter "Create Pet"
-# When I click the link
-# I am taken to '/shelters/:shelter_id/pets/new' where I see a form to add a new adoptable pet
-# When I fill in the form with the pet's:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex ('female' or 'male')
-# And I click the button "Create Pet"
-# Then a `POST` request is sent to '/shelters/:shelter_id/pets',
-# a new pet is created for that shelter,
-# that pet has a status of 'adoptable',
-# and I am redirected to the Shelter Pets Index page where I can see the new pet listed
