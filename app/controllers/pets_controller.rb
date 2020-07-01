b9fd90d86dc6db94be80fa7562cddd7ec6b577bb
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-01 17:23:54 -0600

    Add edit and update actions to the pets controller.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,6 +17,17 @@ class PetsController < ApplicationController
     redirect_to "/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def update
+    @pet = Pet.find(params[:pet_id])
+    @pet.update(pet_params)
+
+    redirect_to "/pets/#{@pet.id}"
+  end
+
   private
   def pet_params
     params.permit(:name, :age, :sex, :image, :description)
