21fd5c544ac90767e8cf1cf711deeee961acd51d
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-30 23:03:29 -0600

    Add pets routes pointiong to the index action in pets controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'shelters#index'
+  root to: 'shelters#index'
   get "/shelters", to: 'shelters#index'
   get "/shelters/new", to: 'shelters#new'
   get "/shelters/:shelter_id", to: 'shelters#show'
@@ -8,4 +10,6 @@ Rails.application.routes.draw do
   post "/shelters", to: 'shelters#create'
   patch "/shelters/:shelter_id", to: 'shelters#update'
   delete "/shelters/:shelter_id", to: 'shelters#destroy'
+
+  get "/pets", to: 'pets#index'
 end
