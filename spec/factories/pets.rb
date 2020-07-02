3cd541d91cb02b6d95c131387d9503ccd8ef6f2e
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-01 23:21:17 -0600

    Update styles.

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     name { Faker::Creature::Dog.name}
     age { Faker::Number.between(from: 1, to: 15)}
     sex { Faker::Gender.short_binary_type.upcase }
-    image { Faker::Avatar.image( size: "300x300", format: "jpg") }
+    image { Faker::Avatar.image( size: "100x100", format: "jpg") }
     description {Faker::Lorem.paragraph(sentence_count: 3)}
   end
 end
